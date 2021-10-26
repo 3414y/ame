4ef751734aea040423f4a36f42c3d53199155f0c
3413 <3413@example.com>
2021-10-26 11:50:43 +0900

    aaaa

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
     include SessionsHelper
+    before_action :logged_in_user
+
+    private
+        def logged_in_user
+            unless logged_in?
+             flash[:danger] = "Please log in"
+             redirect_to login_path
+            end
+        end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_19_014027) do
+ActiveRecord::Schema.define(version: 2021_10_26_024803) do
 
   create_table "gakkas", force: :cascade do |t|
     t.string "gakkamei"
@@ -101,6 +101,7 @@ ActiveRecord::Schema.define(version: 2021_10_19_014027) do
     t.string "simei"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
   end
 
 end
