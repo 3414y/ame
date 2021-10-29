0fe9e888bad899b2f992dc9937d06de2f8a2290d
3414 <3414@example.com>
2021-10-29 11:37:10 +0900

    aaa

--- db/schema.rb
+++ db/schema.rb
@@ -77,7 +77,7 @@ ActiveRecord::Schema.define(version: 2021_10_26_024803) do
   create_table "syuusyokus", force: :cascade do |t|
     t.string "gakkoumei"
     t.integer "gakka_id"
-    t.boolean "kennai"
+    t.string "kennai"
     t.string "shp"
     t.float "hyoutei"
     t.datetime "created_at", precision: 6, null: false
