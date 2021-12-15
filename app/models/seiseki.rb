b99c3877b6d46da31144dfa3424eca1797c429b3
3413 <3413@example.com>
2021-12-15 11:29:51 +0900

    aaa

--- app/models/seiseki.rb
+++ app/models/seiseki.rb
@@ -10,4 +10,22 @@ class Seiseki < ApplicationRecord
            return data.id
         end
     end
+
+    def self.hyouteiheikin(seisekis)
+        sum = 0.0
+        heikin = 0.0
+        kyouka = 0.0
+        seisekis.each do |seiseki|
+          seiseki.tensus.each do |tensu|
+            if tensu.hyoutei.to_i >= 1 
+              sum = sum + tensu.hyoutei.to_i
+              kyouka = kyouka + 1
+            end
+          end
+        end
+        if kyouka >= 1
+          heikin = sum / kyouka
+        end
+        return heikin.round(1)
+    end 
 end
