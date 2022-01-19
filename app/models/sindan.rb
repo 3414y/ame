6fe48dde4c395fe0b003a4371ee87ee3bda5af78
3427 <3427@example.com>
2022-01-19 10:35:22 +0900

    診断画面

--- app/models/sindan.rb
+++ app/models/sindan.rb
@@ -1,6 +1,8 @@
 class Sindan < ApplicationRecord
     #has_many :kigyous, dependent: :destroy
-  
+    belongs_to :syokusyu,optional: true
+    belongs_to :gakka,optional: true
+    
     scope :search, -> (keyword) {
       where('sinrosaki like :q OR kigyoumei like :q', q: "%#{keyword}%") if keyword.present?
     }
